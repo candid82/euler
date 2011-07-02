64a0836e9c9c6aed35d3698e471c1cf49098b41c
Roman Bataev <roman.bataev@gmail.com>
2011-07-01 22:20:36 -0400

    problem 3

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -3,4 +3,17 @@
 (defn divides? [d n] (zero? (mod n d)))
 
 (def fibs
-     (lazy-cat [0 1] (map + (rest fibs) fibs)))
+  (lazy-cat [0 1] (map + (rest fibs) fibs)))
+
+(defn sqrt [n] (Math/sqrt n))
+
+(defn factors [n]
+  (let [next-factor
+        (fn [n p]
+          (cond
+           (> p (sqrt n)) (cons n nil)
+           (divides? p n) (lazy-seq (cons p (next-factor (quot n p) p)))
+           :else (recur n (inc p))))]
+    (next-factor n 2)))
+
+(def unique-factors (comp distinct factors))
--- src/euler/problems.clj
+++ src/euler/problems.clj
@@ -24,3 +24,13 @@
 ;; find the sum of the even-valued terms.
 
 (reduce + (for [i fibs :while (<= i 4000000) :when (even? i)] i))
+
+
+;; Problem 3
+;; 02 November 2001
+
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+
+;; What is the largest prime factor of the number 600851475143 ?
+
+(last (factors 600851475143))
